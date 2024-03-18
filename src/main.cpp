4ac861086522813ccb469f9dc0171cec8944cfca
dmitry buggenhout <dmitry.buggenhout@gmail.com>
2024-03-18 16:46:19 +0100

    camera

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,19 @@
 #include <GLFW/glfw3.h>
 #include <math.h>
 
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/quaternion_geometric.hpp>
+#include <glm/ext/vector_float3.hpp>
+#include <glm/geometric.hpp>
+
 #include "../third_party/fmt/include/fmt/core.h"
 #include "shader.hpp"
 #define STB_IMAGE_IMPLEMENTATION
 // #include <stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "../third_party/stb_image/stb_image.h"
 
 static constexpr int WIDTH = 1920 / 2;
@@ -30,18 +39,51 @@ int main() {
 
     // data
 
-    float vertices[] = {
-        0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
-        0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
-        -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
-    };
+    float vertices[] = {0.5f,
+                        0.5f,
+                        0.0f,
+                        1.0f,
+                        0.0f,
+                        0.0f,
+                        1.0f,
+                        1.0f,  // top right
+                        0.5f,
+                        -0.5f,
+                        0.0f,
+                        0.0f,
+                        1.0f,
+                        0.0f,
+                        1.0f,
+                        0.0f,  // bottom right
+                        -0.5f,
+                        -0.5f,
+                        0.0f,
+                        0.0f,
+                        0.0f,
+                        1.0f,
+                        0.0f,
+                        0.0f,  // bottom left
+                        -0.5f,
+                        0.5f,
+                        0.0f,
+                        1.0f,
+                        1.0f,
+                        0.0f,
+                        0.0f,
+                        1.0f  // top left
+                            - 0.5f,
+                        0.5f,
+                        0.5f,
+                        1.0f,
+                        1.0f,
+                        0.0f,
+                        0.0f,
+                        1.0f};
 
-    unsigned int indices[] = {0, 1, 2, 2, 3, 0};
+    unsigned int indices[] = {0, 1, 2, 2, 3, 0, 0, 1, 4};
 
     // read shaders
     Shader shader{"../shaders/basic_shader.vert", "../shaders/basic_shader.frag"};
-
 
     // textures
     stbi_set_flip_vertically_on_load(true);
@@ -62,7 +104,6 @@ int main() {
     glGenerateMipmap(GL_TEXTURE_2D);
 
     stbi_image_free(data);
-
 
     data = stbi_load("../assets/face.png", &width, &height, &nrChannels, 0);
     unsigned int texture1;
@@ -97,19 +138,35 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // coords
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);  // coords
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))); // colors
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));  // colors
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float))); // texuture
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));  // texuture
     glEnableVertexAttribArray(2);
 
     // wireframe mode
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+    glEnable(GL_DEPTH_TEST);
+
+    // space
+
+    glm::mat4 model = glm::mat4(1.0f);
+
+    glm::mat4 view;
+    glm::vec3 camera_position{0.0f, 0.0f, 3.0f};
+    glm::vec3 camera_front{0.0f, 0.0f, -1.0f};
+    glm::vec3 camera_up{0.0f, 1.0f, 0.0f};
+    view = glm::lookAt(camera_position, camera_position + camera_front, camera_up);
+
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    // prepare draw call
     shader.use();
     shader.setFloat("mix_value", 0.1);
-    glUniform1i(glGetUniformLocation(shader.ID, "texture0"), 0);
+    shader.setInt("texture0", 0);
     shader.setInt("texture1", 1);
 
     while (!glfwWindowShouldClose(window)) {
@@ -117,6 +174,29 @@ int main() {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window, true);
         }
+
+        const float cameraSpeed = 0.05f;  // adjust accordingly
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) camera_position += cameraSpeed * camera_front;
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) camera_position -= cameraSpeed * camera_front;
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+            camera_position -= glm::normalize(glm::cross(camera_front, camera_up)) * cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+            camera_position += glm::normalize(glm::cross(camera_front, camera_up)) * cameraSpeed;
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // model = glm::rotate(model, (float)glfwGetTime() / 800.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+        // camera_position = glm::vec3(3.0f * sin(glfwGetTime()), camera_position.y, 3.0f * cos(glfwGetTime()));
+    view = glm::lookAt(camera_position, camera_position + camera_front, camera_up);
+
+        unsigned int model_location = glGetUniformLocation(shader.ID, "model");
+        glUniformMatrix4fv(model_location, 1, GL_FALSE, glm::value_ptr(model));
+
+        unsigned int view_location = glGetUniformLocation(shader.ID, "view");
+        glUniformMatrix4fv(view_location, 1, GL_FALSE, glm::value_ptr(view));
+
+        unsigned int projection_location = glGetUniformLocation(shader.ID, "projection");
+        glUniformMatrix4fv(projection_location, 1, GL_FALSE, glm::value_ptr(projection));
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -127,7 +207,7 @@ int main() {
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
         // check and call events and swap buffers
         glfwPollEvents();
