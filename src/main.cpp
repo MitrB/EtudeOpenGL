f2b71cab4e5ecbf3dce4c375b2200cfa97d9954a
dmitry buggenhout <dmitry.buggenhout@gmail.com>
2024-03-18 01:29:42 +0100

    dreaming of triangles

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,95 @@
-#include "../third_party/fmt/include/fmt/core.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
 
+#include "../third_party/fmt/include/fmt/core.h"
+#include "shader.hpp"
+
+static constexpr int WIDTH = 1920 / 2;
+static constexpr int HEIGHT = 1080 / 2;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) { glViewport(0, 0, width, height); }
 
 int main() {
     fmt::print("Hello OpenGl\n");
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "EtudeGL", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // data
+
+    float vertices[] = {
+        // positions         // colors
+        0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom left
+        0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f   // top
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2
+    };
+
+    // read shaders
+    Shader shader{"../shaders/basic_shader.vert", "../shaders/basic_shader.frag"};
+
+    // buffers
+    unsigned int VBO;
+    unsigned int EBO;
+    unsigned int VAO;
+
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // wireframe mode
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Input
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        float time_value = glfwGetTime();
+        float gree_value = (sin(time_value) / 2.0f) + 0.5f;
+        int vertex_color_location = glGetUniformLocation(shader.ID, "vertexColor");
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // rendering
+        shader.use();
+        glUniform4f(vertex_color_location, 0.0f, gree_value, 0.0f, 1.0f);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+        // check and call events and swap buffers
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
